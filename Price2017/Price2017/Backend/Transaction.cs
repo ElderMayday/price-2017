8f8aa775b5a5275114b88abaa15d9284b7735efa
ElderMayday <galdarsar@gmail.com>
2017-06-23 20:57:20 +0200

    Add TransactionContainer and Action

---
+++ Price2017/Price2017/Backend/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price2017.Backend
+{
+    class Transaction
+    {
+        public DateTime Time { get; set; }
+        int NameId { get; set; }
+        double Price { get; set; }
+        double Amount { get; set; }
+        bool IsBut { get; set; }
+    }
+}
---
+++ Price2017/Price2017/Backend/TransactionContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price2017.Backend
+{
+    class TransactionContainer
+    {
+        SortedDictionary<long, Transaction> transactions;
+        Dictionary<int, string> names;
+    }
+}
